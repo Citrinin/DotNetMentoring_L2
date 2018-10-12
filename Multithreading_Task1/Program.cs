410dbb45510e1f37ec04e297a92a66849d8fe33d
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-10-12 15:16:32 +0300

    [Task 1]  Add task1

--- Multithreading_Task1/Program.cs
+++ Multithreading_Task1/Program.cs
@@ -10,6 +10,26 @@ namespace Multithreading_Task1
     {
         static void Main(string[] args)
         {
+            var tasksList = new List<Task>();
+
+            for (var i = 0; i < 100; i++)
+            {
+                tasksList.Add(PrinterTask(i));
+            }
+
+            Task.WaitAll(tasksList.ToArray());
+            Console.WriteLine("Finish");
+        }
+
+        public static Task PrinterTask(int sequenceNumber)
+        {
+            return Task.Run(() =>
+            {
+                for (var j = 0; j < 1000; j++)
+                {
+                    Console.WriteLine($"Task #{sequenceNumber} – {j}");
+                }
+            });
         }
     }
 }
