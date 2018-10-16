60d6f452344d1a3853b7c5748b93b02a506c089e
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-10-16 20:59:02 +0300

    [Task 5] Fix task with semaphore

--- Multithreading_Task5/Program.cs
+++ Multithreading_Task5/Program.cs
@@ -1,34 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Multithreading_Task5
 {
     class Program
     {
-        private static Semaphore _syncSemaphore = new Semaphore(0, 1);
+        private static Semaphore _syncSemaphore = new Semaphore(1, 1);
 
         static void Main(string[] args)
         {
             ThreadPool.QueueUserWorkItem(ThreadCallback, new ThreadParameter { Counter = 10, Number = 100 });
-            _syncSemaphore.WaitOne();
+            Console.ReadKey();
         }
 
         static void ThreadCallback(object param)
         {
             var parameter = (ThreadParameter)param;
             Console.WriteLine($"Current thread {Environment.CurrentManagedThreadId}, thread number {parameter.Counter}, current number {parameter.Number}");
+
+            _syncSemaphore.WaitOne();
             parameter.Counter--;
             parameter.Number--;
             if (parameter.Counter > 0)
             {
                 ThreadPool.QueueUserWorkItem(ThreadCallback, parameter);
-                _syncSemaphore.WaitOne();
             }
             _syncSemaphore.Release();
+            Console.WriteLine($"Current thread {Environment.CurrentManagedThreadId}");
         }
 
         class ThreadParameter
