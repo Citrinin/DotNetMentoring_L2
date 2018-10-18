408fe371f4b3c3c943c0d0535e05456d7dc9346a
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-10-18 14:52:14 +0300

    [Task 4] Add person, personRepository classes

---
+++ Async-Await_Task4/PersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Async_Await_Task4
+{
+    public class PersonRepository
+    {
+        private readonly IDbConnection _dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        public List<Person> GetAll()
+        {
+            return _dbConnection.Query<Person>("SELECT * FROM Persons").ToList();
+        }
+    }
+}
---
+++ Async-Await_Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace Async_Await_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conString = ConfigurationManager.ConnectionStrings["defaultConnection"]
+                .ConnectionString;
+        }
+    }
+}
