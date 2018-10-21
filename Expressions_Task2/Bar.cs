baf1b56e85afd819e21630cf5994db29bf1cc365
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-10-21 20:57:07 +0300

    [Task 2] Add mapper

---
+++ Expressions_Task2/Bar.cs
@@ -0,0 +1,14 @@
+﻿namespace Expressions_Task2
+{
+    public class Bar
+    {
+        public int IntProperty { get; set; }
+        public string StringProperty { get; set; }
+        public decimal DecimalProperty { get; set; }
+
+        public override string ToString()
+        {
+            return $"Bar instance:\n{nameof(IntProperty)} - {IntProperty}\n{nameof(StringProperty)} - {StringProperty}\n{nameof(DecimalProperty)} - {DecimalProperty}\n";
+        }
+    }
+}
