e536f0b1e6c30ffecb5b1fa940cf2dec980062b0
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-10-26 17:49:44 +0300

    [Task 1] Add base of the project

---
+++ PowerStateManaged/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerStateManaged
+{
+    public static class PowerStateManaged
+    {
+        [DllImport("powrprof.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "1")]
+        public static extern int GetPowerInfo(byte informationLevel, UInt64 lpInBuffer, UInt32 inBufferSize,
+            UInt64 lpOutBuffer, UInt32 outBufferSize);
+    }
+}
