e1c0150cce3a9f6ca080e951c364252bd35e2fea
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-11-30 12:25:48 +0300

    [Task 1] Add validator for xml file

--- LibrarySavior/Program.cs
+++ LibrarySavior/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace LibrarySavior
 {
@@ -10,6 +12,21 @@ namespace LibrarySavior
     {
         static void Main(string[] args)
         {
+            XmlReaderSettings settings = new XmlReaderSettings();
+
+            settings.Schemas.Add("http://library.by/catalog", "../../books_schema.xsd");
+            settings.ValidationEventHandler += (sender, eventArgs) =>
+            {
+                Console.WriteLine($"On {eventArgs.Exception.LineNumber} line at {eventArgs.Exception.LinePosition} position occured error:\n\t {eventArgs.Message}\n");
+            };
+
+            settings.ValidationFlags = settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationType = ValidationType.Schema;
+
+            XmlReader reader = XmlReader.Create("../../books.xml", settings);
+
+            while (reader.Read());
+            Console.ReadKey();
         }
     }
 }
